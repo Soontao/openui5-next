5825f9f41bc5c2198d6b976b0156d628e55b23a2
Theo Sun <theo.sun@outlook.com>
2018-11-14 23:02:46 +0800

    feat(class): push almost all module to es6 class format

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,78 @@
-var path = require('path');
-var babel = require('gulp-babel');
+var path = require("path");
+var babel = require("gulp-babel");
 var gulp = require("gulp");
-var fs = require("fs")
-var less = require('gulp-less');
-var del = require('del');
-var { filter, includes, concat } = require("lodash")
+var fs = require("fs");
+var less = require("gulp-less");
+var del = require("del");
+var { filter, includes, concat } = require("lodash");
 
-var srcRoot = path.join(__dirname, "./src")
-var themeModules = ["themelib_sap_belize", "themelib_sap_bluecrystal"]
-var testModules = ["testsuite", "testsuite-utils"]
-var ignoreModules = concat(themeModules, testModules, ["sap.ui.demokit", "sap.ui.codeeditor"])
-var modules = fs.readdirSync(srcRoot)
-var jsModules = filter(modules, d => !includes(ignoreModules, d))
+var srcRoot = path.join(__dirname, "./src");
+var themeModules = ["themelib_sap_belize", "themelib_sap_bluecrystal"];
+var testModules = ["testsuite", "testsuite-utils"];
+var ignoreModules = concat(themeModules, testModules, [
+  "sap.ui.demokit",
+  "sap.ui.codeeditor"
+]);
+var modules = fs.readdirSync(srcRoot);
+var jsModules = filter(modules, d => !includes(ignoreModules, d));
 
-gulp.task("copy:thirdpartylib", () => gulp.src("./src/sap.ui.core/src/sap/ui/thirdparty/**/*").pipe(gulp.dest("./dist/sap/ui/thirdparty/")))
+gulp.task("copy:thirdpartylib", () =>
+  gulp
+    .src("./src/sap.ui.core/src/sap/ui/thirdparty/**/*")
+    .pipe(gulp.dest("./dist/sap/ui/thirdparty/"))
+);
 
-gulp.task("copy:properties", () => gulp.src(modules.map(m => `./src/${m}/src/**/*.properties`)).pipe(gulp.dest("./dist/")))
+gulp.task("copy:properties", () =>
+  gulp
+    .src(modules.map(m => `./src/${m}/src/**/*.properties`))
+    .pipe(gulp.dest("./dist/"))
+);
 
-gulp.task("forward:es6module", () => gulp.src(
-  concat(jsModules.map(m => `src/${m}/src/**/*.js`), "!src/sap.ui.core/src/sap/ui/thirdparty/**/*")).pipe(babel()).pipe(gulp.dest("./dist/"))
-)
+gulp.task("forward:es6module", () =>
+  gulp
+    .src(
+      concat(
+        jsModules.map(m => `src/${m}/src/**/*.js`),
+        "!src/sap.ui.core/src/sap/ui/thirdparty/**/*"
+      )
+    )
+    .pipe(babel())
+    .pipe(gulp.dest("./dist/"))
+);
 
-gulp.task("less", gulp.series(
-  function copyLess() { return gulp.src(concat(themeModules, jsModules).map(m => `src/${m}/src/**/*.{less,css,json,woff,ttf,woff2,TTF}`)).pipe(gulp.dest("./dist/")) },
-  function compileLess() { return gulp.src(concat(themeModules, jsModules).map(m => `./dist/${m.replace(/\./g, "/")}/**/library.source.less`)).pipe(less()).pipe(gulp.dest("./dist/")) }
-))
+gulp.task(
+  "less",
+  gulp.series(
+    function copyLess() {
+      return gulp
+        .src(
+          concat(themeModules, jsModules).map(
+            m => `src/${m}/src/**/*.{less,css,json,woff,ttf,woff2,TTF}`
+          )
+        )
+        .pipe(gulp.dest("./dist/"));
+    },
+    function compileLess() {
+      return gulp
+        .src(
+          concat(themeModules, jsModules).map(
+            m => `./dist/${m.replace(/\./g, "/")}/**/library.source.less`
+          )
+        )
+        .pipe(less())
+        .pipe(gulp.dest("./dist/"));
+    }
+  )
+);
 
-gulp.task("clean", () => { return del("./dist") })
+gulp.task("clean", () => {
+  return del("./dist");
+});
 
-gulp.task('build', gulp.series("clean", gulp.parallel("copy:thirdpartylib", "forward:es6module", "less")));
+gulp.task(
+  "build",
+  gulp.series(
+    "clean",
+    gulp.parallel("copy:thirdpartylib", "forward:es6module", "less")
+  )
+);
--- internal/ui5-module-to-es6-module.js
+++ internal/ui5-module-to-es6-module.js
@@ -1,48 +1,171 @@
-const { get, isEmpty, concat, filter, size, zipObject, map, min, split, lastIndexOf, join, slice } = require("lodash");
+const {
+  get,
+  isEmpty,
+  concat,
+  filter,
+  size,
+  zipObject,
+  map,
+  min,
+  split,
+  lastIndexOf,
+  join,
+  slice
+} = require("lodash");
 const { readFileSync, existsSync } = require("fs");
-const { warn } = require("console")
-const { dirname, relative, join: pJoin } = require("path")
-const { type } = require("os")
+const { warn } = require("console");
+const { dirname, relative, join: pJoin } = require("path");
+const { type } = require("os");
 
-module.default = (babel) => {
-
+module.default = babel => {
   const { types: t } = babel;
 
   const isRelativeModuleImport = (m = "") => {
-    return m.startsWith("./") || m.startsWith("../")
-  }
+    return m.startsWith("./") || m.startsWith("../");
+  };
 
-  const getLatestSAPFolderAbsPath = (filepath) => {
+  const getLatestSAPFolderAbsPath = filepath => {
     if (existsSync(pJoin(dirname(filepath), "sap"))) {
-      return filepath
+      return filepath;
     } else {
-      const pathSequenses = split(filepath, "/")
-      const lastSAPDIRIndex = lastIndexOf(pathSequenses, "sap")
-      const latestSAPFolder = join(slice(pathSequenses, 0, lastSAPDIRIndex + 1), "/")
-      return latestSAPFolder
+      const pathSequenses = split(filepath, "/");
+      const lastSAPDIRIndex = lastIndexOf(pathSequenses, "sap");
+      const latestSAPFolder = join(
+        slice(pathSequenses, 0, lastSAPDIRIndex + 1),
+        "/"
+      );
+      return latestSAPFolder;
     }
-  }
+  };
+
+  const mapObjectToClassBody = objectExpression => {
+    if (!objectExpression) {
+      throw new Error("must input an AST node");
+    }
+    if (objectExpression.type != "ObjectExpression") {
+      throw new Error(
+        `must input ObjectExpression node but input ${
+          objectExpression.type
+        } node`
+      );
+    }
+    var classBodyItems = map(objectExpression.properties, prop => {
+      switch (prop.value.type) {
+        case "FunctionExpression":
+          var funcExpression = prop.value;
+          var methodType =
+            prop.key.name == "constructor" ? "constructor" : "method";
+          return t.classMethod(
+            methodType,
+            prop.key,
+            funcExpression.params,
+            funcExpression.body,
+            false
+          );
+        default:
+          return t.classProperty(prop.key, prop.value);
+      }
+    });
+    return t.classBody(classBodyItems);
+  };
 
   const visitor = {
     Program: {
       enter: path => {
         if (!path.state) {
-          path.state = {}
+          path.state = {};
         }
-        path.state.ui5next = { inserts: [] }
+        path.state.ui5next = { inserts: [] };
       },
       exit: path => {
-        var state = path.state.ui5next
+        var state = path.state.ui5next;
         if (!isEmpty(state.inserts)) {
-          path.node.body = concat(path.node.body, state.inserts)
+          path.node.body = concat(path.node.body, state.inserts);
+        }
+      }
+    },
+
+    VariableDeclaration: {
+      enter: path => {
+        var node = path.node;
+
+        // is Object.extend class expression
+        if (
+          get(node, "declarations[0].init.callee.property.name", "") == "extend"
+        ) {
+          var declaration = node.declarations[0];
+          var superClassIndentifier = declaration.init.callee.object;
+          var classNameIndentifier = declaration.id;
+          var args = declaration.init.arguments;
+          var prefix = `class ${classNameIndentifier.name} extends ${
+            superClassIndentifier.name
+          }`;
+          var warnning = str => {
+            warn(`${prefix}: ${str}`);
+          };
+          var replaced;
+          var body;
+          if (
+            superClassIndentifier.name == "jQuery" ||
+            superClassIndentifier.name == "jQueryDOM" ||
+            superClassIndentifier.name == "$"
+          ) {
+            // ignore jQuery.extend
+            return;
+          }
+
+          switch (size(args)) {
+            case 1:
+              body = args[0];
+              break;
+            case 2:
+              body = args[1];
+              break;
+            default:
+              warnning(`unsupported args size`);
+              break;
+          }
+
+          if (!body) {
+            warnning(`not have class body, so ignore it`);
+          } else {
+            switch (body.type) {
+              case "ObjectExpression":
+                replaced = t.classDeclaration(
+                  classNameIndentifier,
+                  superClassIndentifier,
+                  mapObjectToClassBody(body),
+                  []
+                );
+                break;
+              case "Identifier":
+                warnning(`not support convert because it extend an Identifier`);
+                break;
+              case "StringLiteral":
+                //  ClientPropertyBinding.extend("sap.ui.model.xml.XMLPropertyBinding");
+                replaced = t.classDeclaration(
+                  classNameIndentifier,
+                  superClassIndentifier,
+                  t.classBody([]),
+                  []
+                );
+                break;
+              default:
+                warnning(`unsupported expression ${body.type}`);
+                break;
+            }
+          }
+
+          if (!isEmpty(replaced)) {
+            path.replaceWith(replaced);
+          }
         }
       }
     },
 
     CallExpression: {
-      enter: path => {
+      exit: path => {
         var node = path.node;
-        var parentNode = path.parent;
 
         // is sap.ui.define call expression
         if (
@@ -50,19 +173,22 @@ module.default = (babel) => {
           get(node, ["callee", "object", "property", "name"], "") == "ui" &&
           get(node, ["callee", "property", "name"], "") == "define"
         ) {
-
           var state = path.state.ui5next;
           var filepath = path.hub.file.opts.filename;
           // fix jquery.sap.xxx.js require path
-          var relativePrefix = relative(filepath, getLatestSAPFolderAbsPath(filepath)).replace(/\\/g, "/") || "."
+          var relativePrefix =
+            relative(filepath, getLatestSAPFolderAbsPath(filepath)).replace(
+              /\\/g,
+              "/"
+            ) || ".";
           var args = node.arguments || [];
-          var imports = []
-          var definationFunction
+          var imports = [];
+          var definationFunction;
 
           args.forEach(arg => {
             switch (arg.type) {
               case "FunctionExpression":
-                definationFunction = arg
+                definationFunction = arg;
                 break;
               case "ArrayExpression":
                 imports = arg.elements;
@@ -75,52 +201,55 @@ module.default = (babel) => {
           if (definationFunction) {
             var definationFunctionParams = definationFunction.params;
 
-            var expresions = definationFunction.body.body // is array
+            var expresions = definationFunction.body.body; // is array
 
             if (!isEmpty(expresions)) {
               // convert to require
               if (!isEmpty(imports)) {
-                // add require expressions 
-                var requireExpressions = []
-                for (let index = 0; index < min([size(imports), size(definationFunctionParams)]); index++) {
+                // add require expressions
+                var requireExpressions = [];
+                for (
+                  let index = 0;
+                  index < min([size(imports), size(definationFunctionParams)]);
+                  index++
+                ) {
                   const id = definationFunctionParams[index];
-                  const mName = imports[index]
+                  const mName = imports[index];
                   // if not relative module improt, convert to relative
                   if (!isRelativeModuleImport(mName.value)) {
-                    mName.value = `${relativePrefix}/${mName.value}`
+                    mName.value = `${relativePrefix}/${mName.value}`;
                   }
                   requireExpressions.push(
                     t.importDeclaration([t.importDefaultSpecifier(id)], mName) // es6 import
-                    // t.variableDeclaration(
-                    //   "var", [
-
-                    //     t.variableDeclarator(
-                    //       id,
-                    //       t.callExpression(
-                    //         t.identifier("require"), [module]
-                    //       )
-                    //     )
-                    //   ]
-                    // )
-                  )
+                  );
                 }
 
-                expresions = concat(requireExpressions, expresions)
+                expresions = concat(requireExpressions, expresions);
               }
 
               // >> return Module > export default Module
 
-              var rtStatemnets = filter(expresions, { type: "ReturnStatement" })
+              var rtStatemnets = filter(expresions, {
+                type: "ReturnStatement"
+              });
 
-              var expresions = filter(expresions, e => e.type !== "ReturnStatement")
+              var expresions = filter(
+                expresions,
+                e => e.type !== "ReturnStatement"
+              );
 
               if (size(rtStatemnets) > 1) {
-                warn(`file: ${filepath}, have more than one return statements in sap.ui.deinfe, please process it manual.`)
+                warn(
+                  `file: ${filepath}, have more than one return statements in sap.ui.deinfe, please process it manual.`
+                );
                 return;
               }
 
               if (size(rtStatemnets) == 1) {
-                expresions = concat(expresions, t.exportDefaultDeclaration(rtStatemnets[0].argument))
+                expresions = concat(
+                  expresions,
+                  t.exportDefaultDeclaration(rtStatemnets[0].argument)
+                );
               }
 
               // << return Module > export default Module
@@ -128,60 +257,16 @@ module.default = (babel) => {
               state.inserts = concat(state.inserts, expresions);
               path.remove();
             } else {
-
             }
-
           } else {
-
-          }
-        }
-        // is Object.extend class expression
-        if (get(node, ["callee", "property", "name"], "") == "extend") {
-          var superClassName = node.callee.object.name;
-          var className = parentNode.id.name;
-          var args = node.arguments;
-          var replaced = []
-          var body
-          switch (size(args)) {
-            case 1:
-              body = args[0]
-              break;
-            case 2:
-              body = args[1]
-              break;
-            default:
-              warn(`unsupported args size for class ${className}`)
-              break;
-          }
-
-          switch (body.type) {
-            case "ObjectExpression":
-              replaced.push(
-                t.classDeclaration(
-                  t.identifier(className),
-                  t.identifier(superClassName),
-                  t.classBody([])
-                )
-              )
-              break;
-            case "Identifier":
-
-              break;
-            default:
-              warn(`unsupported expression ${body.type}`)
-              break;
-          }
-          if (!isEmpty(replaced)) {
-            path.replaceWithMultiple(replaced)
           }
         }
       }
     }
-
-  }
+  };
   return {
     visitor: visitor
   };
-}
+};
 
 module.exports = module.default;
